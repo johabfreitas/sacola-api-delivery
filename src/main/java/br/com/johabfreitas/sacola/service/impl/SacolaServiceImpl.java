522e073874db618275ce124fa82a35f46db14502
Johab Freitas <freitasjohab@gmail.com>
2022-10-04 12:50:29 -0300

    Regra de negócios

--- src/main/java/br/com/johabfreitas/sacola/service/impl/SacolaServiceImpl.java
+++ src/main/java/br/com/johabfreitas/sacola/service/impl/SacolaServiceImpl.java
@@ -2,7 +2,9 @@ package br.com.johabfreitas.sacola.service.impl;
 
 import br.com.johabfreitas.sacola.enumeration.FormaPagamento;
 import br.com.johabfreitas.sacola.model.Item;
+import br.com.johabfreitas.sacola.model.Restaurante;
 import br.com.johabfreitas.sacola.model.Sacola;
+import br.com.johabfreitas.sacola.repository.ItemRepository;
 import br.com.johabfreitas.sacola.repository.ProdutoRepository;
 import br.com.johabfreitas.sacola.repository.SacolaRepository;
 import br.com.johabfreitas.sacola.resource.dto.ItemDto;
@@ -17,6 +19,7 @@ import java.util.List;
 public class SacolaServiceImpl implements SacolaService {
     private final SacolaRepository sacolaRepository;
     private final ProdutoRepository produtoRepository;
+    private final ItemRepository itemRespository;
     @Override
     public Item incluirItemNaSacola(ItemDto itemDto) {
         Sacola sacola = verSacola(itemDto.getSacolaId());
@@ -30,12 +33,25 @@ public class SacolaServiceImpl implements SacolaService {
                .sacola(sacola)
                .produto(produtoRepository.findById(itemDto.getProdutoId()).orElseThrow(
                        () -> {
-                           throw new RuntimeException("Essa sacola não existe");
+                           throw new RuntimeException("Esse produto não existe!");
                        }
                 ))
                .build();
 
-        return null;
+       List<Item> itensDaSacola = sacola.getItens();
+       if(itensDaSacola.isEmpty()){
+           itensDaSacola.add(itemParaSerInserido);
+       } else {
+           Restaurante restauranteAtual = itensDaSacola.get(0).getProduto().getRestaurante();
+           Restaurante restauranteDoItemParaAdicionar = itemParaSerInserido.getProduto().getRestaurante();
+           if(restauranteAtual.equals(restauranteDoItemParaAdicionar)){
+               itensDaSacola.add(itemParaSerInserido);
+           } else {
+               throw new RuntimeException("Não é possível adicionar produtos de restaurantes diferentes. Feche a sacola ou esvazie!");
+           }
+       }
+        sacolaRepository.save(sacola);
+        return itemRespository.save(itemParaSerInserido);
     }
 
     @Override
